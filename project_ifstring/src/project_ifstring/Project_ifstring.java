67583e15120fe78872887d0cc74da2f8ddad55f8
ikrima nabila <ikrimanabila98@gmail.com>
2018-05-02 11:46:50 +0700

    new example

---
+++ project_ifstring/src/project_ifstring/Project_ifstring.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_ifstring;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GeeksFarm
+ */
+public class Project_ifstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String jk, nama;
+        Scanner inputan = new Scanner(System.in);
+        System.out.print("nama : ");
+        nama=inputan.nextLine();
+        System.out.print("jenis kelamin [L/P] : ");
+        jk=inputan.nextLine();
+        if ("P".equals(jk)||"p".equals(jk)){
+            System.out.print(nama+"berjenis kelamin perempuan");
+        }
+        else if ("L".equals(jk)||"l".equals(jk)){
+            System.out.print(nama+"berjenis kelamin laki-laki");
+        }
+        else {
+            System.out.print("maaf"+nama+ "jenis kelamin anda tidak terdeteksi");
+        }
+    }
+    
+}
